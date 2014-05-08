d068fc5b66f76cb5817c6dd4e34ea3cdb1cbd331
unknown-user <unknown-user@fe80::b93e:be33:d064:9da6%3>
2014-05-08 16:53:33 +0200

    Added rotating current object

---
+++ Assets/RotateBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBehaviour : MonoBehaviour {
+
+	public float speed;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, speed * Time.deltaTime);
+	}
+}
