145233786bb6e991636f5257fc6005aea5bf5047
Maarten van Elsas <m.vanelsas@student.tudelft.nl>
2014-05-12 14:01:14 +0200

    Created server for handling blockplacement

---
+++ ServerTest/Assets/Client.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+
+	float time = 0;
+
+	void Start (){
+		Debug.Log ("test");
+	}
+	
+	void ConnectToServer() {
+		Network.Connect("127.0.0.1", 20000);
+	}
+	
+	void Update ()
+	{
+		time += Time.deltaTime;
+		if (!Network.isClient) {
+			ConnectToServer();
+
+		}
+	}
+	
+	[RPC] 
+	void PrintText (string text) {
+	}
+}
---
+++ ServerTest/Assets/Server.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Server : MonoBehaviour {
+
+	public GameObject prefab;
+
+	void Start (){
+		LaunchServer ();
+	}
+
+	void OnNetworkInstantiate(NetworkMessageInfo info) {
+		Debug.Log("New object instantiated by " + info.sender);
+	}
+
+	void LaunchServer() {
+		Network.InitializeServer(32, 3825, false);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Debug.Log("New block created");
+			Vector3 test = new Vector3 ((float)(Random.Range (0, 1000)/10.0), (float)(Random.Range (0, 1000)/10.0), (float)(Random.Range (0, 1000)/10.0));
+			Network.Instantiate (prefab, test, prefab.transform.rotation, 1);
+		}
+	}
+
+	[RPC] 
+	void PrintText (string text) {
+		Debug.Log(text);
+	}
+
+	/*
+	[RPC]
+	BlockError PlaceBlock(Vector3 location){
+	}
+
+	[RPC]
+	BlockError RemoveBlock(Vector3 location){	
+	}
+
+	[RPC]
+	Block RequestBlock(){
+	}
+
+	[RPC]
+	void ThrowAwayBlock(){
+	}
+
+	[RPC] 
+	//Decide: magnetic or true heading. Is compas heading already contained in the attitude from gyro?
+	Block Tap(double networkTime, float compasHeading, Quaternion attitude){
+	}
+	*/
+
+	void OnPlayerConnected(NetworkPlayer player) {
+		Debug.Log("Player connected from " + player.ipAddress);
+
+	}
+	
+	void OnServerInitialized() {
+		Debug.Log("Server initialized and ready");
+	}
+}
