1d60c9b2009f04865387e946b9a3ad8ead3b8317
Gert Spek <g.spek94@gmail.com>
2014-05-19 15:25:53 +0200

    gitignore updated, random color reimplemented.

--- Client/Assets/Scripts/PrefabInstantiate.cs
+++ Client/Assets/Scripts/PrefabInstantiate.cs
@@ -9,11 +9,9 @@ public class PrefabInstantiate : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		this.transform.parent = GameObject.Find ("ImageTarget").transform as Transform;
-		/*Vector3 test = new Vector3 (5, 5, 5);
-		this.transform.localScale = test; //GameObject.Find ("Cube").transform.localScale;*/
-		this.renderer.material.color = new Color ((float)(Random.Range (0, 1000) / 1000.0),
-		                                         (float)(Random.Range (0, 1000) / 1000.0),
-		                                         (float)(Random.Range (0, 1000) / 1000.0));
+        this.renderer.material.color = new Color((float)(Random.Range(0, 1000) / 1000.0), 
+            (float)(Random.Range(0, 1000) / 1000.0), 
+            (float)(Random.Range(0, 1000) / 1000.0));
 	}
 	
 	// Update is called once per frame
