a7ee8c14f4639360c9746b276adf3c5af04dc853
Gert Spek <g.spek94@gmail.com>
2014-05-26 10:35:37 +0200

    Refactor: ConnectToServer now takes ip and port argument Add: GUI to connect to server.

--- Client/Assets/Scripts/Client.cs
+++ Client/Assets/Scripts/Client.cs
@@ -9,13 +9,27 @@ public class Client : MonoBehaviour {
 	public int port = 3825;
 
 	void Start (){
-		ConnectToServer ();
+		ConnectToServer (ip, port);
 	}
 	
-	void ConnectToServer() {
+	void ConnectToServer(string ip, int port) {
 		Network.Connect(ip, port);
 
 	}
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(5, 5, Screen.width / 5, Screen.height / 4),"Server information");
+        GUI.Label(new Rect(10, 30, Screen.width / 10, 20), "IP: ");
+        GUI.Label(new Rect(10, 70, Screen.width / 10, 20), "Port: ");
+        ip = GUI.TextField(new Rect(50, 30, Screen.width / 7,20), ip);
+        port = int.Parse(GUI.TextField(new Rect(50, 70, Screen.width / 7, 20), ""+port));
+
+        if(GUI.Button(new Rect(20, 110, Screen.width / 8, 20), "Connect")) {
+            Network.Disconnect();
+            ConnectToServer(ip, port);
+        }
+    }
 
 	[RPC]
 	void PlaceBlock(Vector3 location, Vector3 relativeLocation, NetworkViewID NVI){
