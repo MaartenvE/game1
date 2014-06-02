7760cd373301385df9a4a313e562915ecceaf567
Gert Spek <g.spek94@gmail.com>
2014-06-02 09:39:10 +0200

    rename location.cs -> Location.cs, tests now work properly :)

---
+++ Server/Assets/Scripts/Location.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Location : MonoBehaviour
+{
+
+    public Vector3 index = new Vector3(0, 0, 0);
+
+    [RPC]
+    void UpdateLocation(Vector3 newLocation)
+    {
+        index = newLocation;
+    }
+}
