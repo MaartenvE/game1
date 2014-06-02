2a21033ffe3018b7a4045e0be83268feb41a80e5
Jehan <jehandacamara@hotmail.com>
2014-06-02 10:46:31 +0200

    Added bump and bump matcher tests to server.

---
+++ Server/Assets/Editor/Tests/BumpTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using Moq;
+
+
+[TestFixture]
+public class BumpTest {
+
+	//private Mock <NetworkPlayer> sender = new Mock <NetworkPlayer> ();
+	//private NetworkPlayer sender = new NetworkPlayer();
+	NetworkPlayer sender = new NetworkPlayer();
+	Bump target;
+	[SetUp]
+	public void BumpSetup()
+	{
+		NetworkPlayer sender = new NetworkPlayer();
+		target = new Bump(23.86, 8.95f,sender);
+	}
+	[Test]
+	public void BumpTimeTest()
+	{
+		Assert.AreEqual (23.86, target.Time);
+	}
+
+	[Test]
+	public void BumpForceTest()
+	{
+		Assert.AreEqual (8.95f, target.Force);
+	}
+
+	[Test]
+	public void BumpSenderTest()
+	{
+		Assert.AreEqual (sender, target.Sender);
+	}
+
+
+}
