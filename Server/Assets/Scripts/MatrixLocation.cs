737cf884bb09987d18462d29ed9435516feb131d
Gert Spek <g.spek94@gmail.com>
2014-06-03 15:49:59 +0200

    Rename Location.cs MatrixLocation.cs.

--- Server/Assets/Scripts/MatrixLocation.cs
+++ Server/Assets/Scripts/MatrixLocation.cs
@@ -1,20 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Location : MonoBehaviour {
+public class MatrixLocation : MonoBehaviour {
 	
 	public Vector3 index = new Vector3(0,0,0);
-	
-	// Use this for initialization
-	void Start () {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
 	[RPC]
 	void UpdateLocation(Vector3 newLocation){
 		index = newLocation;
