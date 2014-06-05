58a4105884db54da8a411fa6be84b3f9d3908023
Jehan Da Camara <jehandacamara@hotmail.com>
2014-06-05 11:33:58 +0200

    basic concept for a Start Screen and Connect Screen

---
+++ Client/Assets/Scripts/ConnectScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectScreen : MonoBehaviour {
+    private string ip = "";
+    private string port = "";
+
+    private Rect windowRect = new Rect(20, 20, 120, 50);
+
+    void OnGUI()
+    {
+        ip = GUI.TextField(new Rect(0, 0, 200, 30), ip );
+        port = GUI.TextField(new Rect(0, 50, 200, 30), port);
+
+
+        if (GUI.Button(new Rect(0, 100, 100, 60), "Connect"))
+            if (false)
+                Application.LoadLevel(1);
+            else
+                windowRect = GUI.Window(0, windowRect, WindowFunction, "My Window");
+    }
+
+    void WindowFunction(int windowID)
+    {
+        // Draw any Controls inside the window here
+    }
+}
---
+++ Client/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour
+{
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect((Screen.width / 2) - 25 , (Screen.height /2) - 80, 100, 50), "Join Game"))
+        {
+            Debug.Log("Join game button was clicked");
+        }
+
+       if (GUI.Button(new Rect((Screen.width / 2) -25, (Screen.height / 2) -20, 100, 50), "Spectate"))  
+        {
+            Debug.Log("Spectate button was clicked");
+        }
+            
+        
+    }	
+}
