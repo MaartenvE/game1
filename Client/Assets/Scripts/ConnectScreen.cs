9063fea9e4ab8268ca2aadcb460b16027f1f4422
Jehan Da Camara <jehandacamara@hotmail.com>
2014-06-10 10:50:56 +0200

    Back options fixed for now

--- Client/Assets/Scripts/ConnectScreen.cs
+++ Client/Assets/Scripts/ConnectScreen.cs
@@ -13,13 +13,16 @@ public class ConnectScreen : MonoBehaviour {
         _port = GUI.TextField(new Rect(0, 50, 200, 30), _port);
 
 
-        if (GUI.Button(new Rect(0, 100, 100, 60), "Connect"))
-            if (true){
-                Application.LoadLevel(1);
-            } 
+        if (GUI.Button (new Rect (0, 100, 100, 60), "Connect")) {
+			if (true) {
+					Application.LoadLevel (1);
+				} 
 		//still doesn't create a pop up
-            else
-                windowRect = GUI.Window(0, windowRect, WindowFunction, "My Window");
+       		else
+					windowRect = GUI.Window (0, windowRect, WindowFunction, "My Window");
+		}
+
+
     }
 
     void WindowFunction(int windowID)
