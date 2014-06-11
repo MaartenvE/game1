69be29df82dc3d142f6123d8ba80be20db3e4f2a
Niels Bakker <ncb21992@hotmail.com>
2014-06-11 18:06:03 +0200

    added correct cameramovement for the serverscene

--- Server/Assets/Scripts/CameraMovement.cs
+++ Server/Assets/Scripts/CameraMovement.cs
@@ -8,12 +8,10 @@ public class CameraMovement : MonoBehaviour {
 	public float MovementSpeed;
 
 	private float currentAngle;
-	 //float circleLength;
 	private Vector3 RadiusVector;
 
 	// Use this for initialization
 	void Start () {
-		//circleLength = 2 * Mathf.PI * RadiusOfMove;
 		float x = RadiusOfMove * Mathf.Cos (45);
 		float z = RadiusOfMove * Mathf.Sin (45);
 		RadiusVector = new Vector3 (x,0,z);
@@ -29,7 +27,7 @@ public class CameraMovement : MonoBehaviour {
 
 		currentAngle += MovementSpeed;
 		camera.transform.position = CenterOfMove+rotateVector (RadiusVector, currentAngle);
-		GameObject testblock = GameObject.FindGameObjectWithTag("testcube");
+
 		camera.transform.LookAt (getCameraRotationTo(CenterOfMove));
 
 	}
@@ -43,7 +41,7 @@ public class CameraMovement : MonoBehaviour {
 	}
 
 	public Vector3 getCameraRotationTo(Vector3 vector){
-		return new Vector3(50,0,50);
+		return new Vector3(vector.x,0,vector.z);
 	}
 
 
